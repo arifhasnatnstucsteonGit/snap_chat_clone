195f3ab253394ab33337c251481e795fd261eba0
arifhasnatnstucsteonGit <arifhasnat.info@gmail.com>
2017-09-08 08:57:45 +0600

    project created

---
+++ SnapChatClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChatClone
+//
+//  Created by Arif on 9/8/17.
+//  Copyright © 2017 Arif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
